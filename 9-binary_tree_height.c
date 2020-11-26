79d9420ad74c04954e232ed0512cec451ac58ec1
stivenz <1806@holbertonschool.com>
2020-11-26 17:31:17 -0500

    compute height of a binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * height - computes height
+ * @tree: Pointer to tree binary
+ * Return: height of the binary tree
+ */
+size_t height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (((height(tree->left) > height(tree->right)) ? height(tree->left) + 1 : height(tree->right)) + 1);
+}
+
+/**
+ * binary_tree_height - computes height
+ * @tree: Pointer to tree binary
+ * Return: height of the binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	return (height(tree) - 1);
+}
