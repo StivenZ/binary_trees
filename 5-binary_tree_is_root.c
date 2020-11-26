dac9dee056d652880d25476c958116dfbc1b6331
crua0316 <cresrugi@gmail.com>
2020-11-25 21:27:13 -0500

    Try fix tak 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,16 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - checks if node is leaf
+ * binary_tree_is_root - checks if node is leaf
  * @node: Pointer to node to check
  * Return: 1 if node, 0 if not
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (!node)
-        return (0);
-    if (!node->parent)
-        return (1);
-    return (0);
+	if (!node)
+		return (0);
+	else if (!node->parent)
+		return (1);
+	else
+		return (0);
 }
