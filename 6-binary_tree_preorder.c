656bc7121968497e91ea6d69320db4f5cd8b621f
stivenz <1806@holbertonschool.com>
2020-11-25 21:40:07 -0500

    traverse in pre-order

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - traverses in pre-order a binary tree
+ * @tree: Pointer to tree binary
+ * @func: call a print function
+ * Return: Nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (!func || !tree)
+        return;
+
+        func(tree->n);
+        binary_tree_preorder(tree->left, func);
+        binary_tree_preorder(tree->right, func);
+}
