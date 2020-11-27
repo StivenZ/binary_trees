d1fd18bf9ca956128547c1b517fc322d35dbf789
stivenz <1806@holbertonschool.com>
2020-11-27 00:16:04 -0500

    Fixed bug

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,21 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_sibling - finds sibling node
+ * @node: pointer to node to check sibling
+ * Return: pointer to sibling, or NULL if none
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
+
+/**
  * binary_tree_uncle - finds the uncle of a node
  * @node: pointer to node
  * Return: pointer to uncle node of the given node
